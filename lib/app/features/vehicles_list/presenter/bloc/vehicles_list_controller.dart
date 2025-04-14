047b2f1032d34fc86fdcd7ffc47a8d6da6325906
Usuário <kevinw.souza@hotmail.com>
2025-04-14 17:03:31 -0300

    initial bluetooth commit

--- lib/app/features/vehicles_list/presenter/bloc/vehicles_list_controller.dart
+++ lib/app/features/vehicles_list/presenter/bloc/vehicles_list_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:frotalog_gestor_v2/app/features/vehicles_list/presenter/bloc/vehicles_list_state.dart';
 import 'package:frotalog_gestor_v2/app/utils/ecubit.dart';
 
@@ -11,7 +12,77 @@ class VehiclesListController extends ECubit<VehiclesListState> {
       await Future.delayed(const Duration(seconds: 2));
       emit(VehiclesListSuccessState());
     } catch (e) {
-      emit(VehiclesListErrorState());
+      emit(VehiclesListErrorState(message: 'Nenhum veículo encontrado.'));
+    }
+  }
+
+  Future<void> connectToDevice(String platformName) async {
+    emit(VehiclesListLoading());
+
+    try {
+      // Verifica se o Bluetooth está ligado
+      final isBluetoothEnabled =
+          await FlutterBluePlus.adapterState.first == BluetoothAdapterState.on;
+
+      if (!isBluetoothEnabled) {
+        emit(VehiclesListErrorState(
+            message:
+                'Bluetooth está desligado. Por favor, ligue o Bluetooth.'));
+        return;
+      }
+
+      // Obtém a lista de dispositivos já emparelhados
+      final List<BluetoothDevice> bondedDevices =
+          await FlutterBluePlus.bondedDevices;
+
+      if (bondedDevices.isEmpty) {
+        emit(VehiclesListErrorState(
+            message: 'Nenhum dispositivo emparelhado encontrado.'));
+        return;
+      }
+
+      // Encontra o dispositivo correspondente ao platformName
+      final BluetoothDevice? matchedDevice = bondedDevices.firstWhere(
+        (device) => device.platformName == platformName,
+        orElse: () =>
+            throw Exception('Dispositivo correspondente não encontrado.'),
+      );
+
+// Verifica se o dispositivo foi encontrado
+      if (matchedDevice == null) {
+        emit(VehiclesListErrorState(
+            message: 'Dispositivo correspondente não encontrado.'));
+        return;
+      }
+
+      // Conecta ao dispositivo correspondente
+      emit(VehiclesListLoading(
+          message:
+              'Conectando ao dispositivo ${matchedDevice.platformName}...'));
+
+      await matchedDevice.connect(); // Realiza a conexão com o dispositivo
+
+      emit(VehiclesListSuccessState(
+        message: 'Conectado ao dispositivo: ${matchedDevice.platformName}',
+      ));
+    } catch (e) {
+      emit(VehiclesListErrorState(message: 'Erro ao conectar: $e'));
     }
   }
 }
+
+
+
+
+
+
+
+
+
+/*
+bool permissionsGranted = await PermissionManagerBluetooth().requestPermissions();
+  if (!permissionsGranted) {
+    emit(DetailsScreenErrorState(message: 'Permissões não concedidas.'));
+    return false;
+  }
+  */
--- lib/app/features/vehicles_list/presenter/bloc/vehicles_list_state.dart
+++ lib/app/features/vehicles_list/presenter/bloc/vehicles_list_state.dart
@@ -2,8 +2,19 @@ sealed class VehiclesListState {}
 
 final class VehiclesListInitialState extends VehiclesListState {}
 
-final class VehiclesListLoading extends VehiclesListState {}
+final class VehiclesListLoading extends VehiclesListState {
+  final String? message; 
 
-final class VehiclesListErrorState extends VehiclesListState {}
+  VehiclesListLoading({this.message});
+}
 
-final class VehiclesListSuccessState extends VehiclesListState {}
+final class VehiclesListErrorState extends VehiclesListState {
+  final String message; 
+  VehiclesListErrorState({required this.message});
+}
+
+final class VehiclesListSuccessState extends VehiclesListState {
+  final String? message;
+
+  VehiclesListSuccessState({this.message});
+}
